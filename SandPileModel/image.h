5bc323730d8c5fc2f2cf7eb0b48c0c8b22888f6d
1sukharik <144956966+1sukharik@users.noreply.github.com>
2024-04-15 12:20:34 +0300

    Create image.h

---
+++ SandPileModel/image.h
@@ -0,0 +1,8 @@
+#ifndef LABWORK32_LABWORK3_1SUKHARIK_IMAGE_H_
+#define LABWORK32_LABWORK3_1SUKHARIK_IMAGE_H_
+
+#include "parcer.h"
+void MakingImage(Matrix& matriks, Arguments& parameters, int number_of_bmp);
+
+
+#endif //LABWORK32_LABWORK3_1SUKHARIK_IMAGE_H_
