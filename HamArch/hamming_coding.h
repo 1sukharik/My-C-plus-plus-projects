0094c1535e1b28035565ddd81ffdc4067001edac
1sukharik <144956966+1sukharik@users.noreply.github.com>
2024-04-15 12:49:55 +0300

    Create hamming_coding.h

---
+++ HamArch/hamming_coding.h
@@ -0,0 +1,15 @@
+
+
+#ifndef LABWORK6_1SUKHARIK__HAMMING_CODING_H_
+#define LABWORK6_1SUKHARIK__HAMMING_CODING_H_
+
+#include "iostream"
+#include "cmath"
+
+std::string CheckBlock(std::string& a);
+std::string CheckHamming(std::string& a, int size_of_block);
+std::string SetBits(std::string& a);
+std::string MakeHamming(std::string& a, int size_of_block);
+
+
+#endif //LABWORK6_1SUKHARIK__HAMMING_CODING_H_
