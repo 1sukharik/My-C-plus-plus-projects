1a3a3ae2f3242e7c101f28bb1f8cdf612996ed2e
1sukharik <144956966+1sukharik@users.noreply.github.com>
2024-04-15 12:51:11 +0300

    Create parcer.h

---
+++ HamArch/parcer.h
@@ -0,0 +1,16 @@
+
+#ifndef LABWORK6_1SUKHARIK__PARCER_H_
+#define LABWORK6_1SUKHARIK__PARCER_H_
+
+#include "iostream"
+#include <fstream>
+#include <cstdint>
+#include "cstring"
+#include "vector"
+#include <bitset>
+
+
+
+void Parsing(int argc, char **argv);
+
+#endif //LABWORK6_1SUKHARIK__PARCER_H_
